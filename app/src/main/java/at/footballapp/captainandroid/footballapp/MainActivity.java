d779a57652ef0c46dca75dac571a4eb1b45c9c46
Pascal-AUT <laggerpas@gmail.com>
2017-03-20 15:12:09 +0100

    test commit by Pascal (added crucial comments)

--- app/src/main/java/at/footballapp/captainandroid/footballapp/MainActivity.java
+++ app/src/main/java/at/footballapp/captainandroid/footballapp/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //test comment for first commit
+        //what is all the code about ?
     }
 }
