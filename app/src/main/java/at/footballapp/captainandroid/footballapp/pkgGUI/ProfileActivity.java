f8932276c1b8b9ed425ae0952b7dd08dddd71aba
Sasa <sarkovicsasa123@gmail.com>
2017-03-30 14:31:45 +0200

    Added some Activities

--- app/src/main/java/at/footballapp/captainandroid/footballapp/pkgGUI/ProfileActivity.java
+++ app/src/main/java/at/footballapp/captainandroid/footballapp/pkgGUI/ProfileActivity.java
@@ -2,14 +2,21 @@ package at.footballapp.captainandroid.footballapp.pkgGUI;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import at.footballapp.captainandroid.footballapp.R;
 
-public class RemovePlayerGUI extends AppCompatActivity {
+public class ProfileActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_remove_player_gui2);
+        setContentView(R.layout.activity_profile);
+    }
+
+    public void onBtnSave(View view) {
+    }
+
+    public void onBtnReset(View view) {
     }
 }
