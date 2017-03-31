20cea540173797147496490eed0b9978e6357062
JakobWeiler <jakob.weiler@edu.htl-villach.at>
2017-03-31 09:57:52 +0200

    Merge branch 'master' of C:\Users\Jakob\Desktop\HTL\4BHIFS\FootballApp\FootballApp with conflicts.

--- app/src/main/java/at/footballapp/captainandroid/footballapp/pkgGUI/MainActivity.java
+++ app/src/main/java/at/footballapp/captainandroid/footballapp/pkgGUI/MainActivity.java
@@ -1,10 +1,14 @@
-package at.footballapp.captainandroid.footballapp.pkgGUI;
+package at.footballapp.captainandroid.footballapp.pkgMisc;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 
 import at.footballapp.captainandroid.footballapp.R;
+import at.footballapp.captainandroid.footballapp.pkgGUI.EditTeamActivity;
+import at.footballapp.captainandroid.footballapp.pkgGUI.RemoveMatchActivity;
+import at.footballapp.captainandroid.footballapp.pkgGUI.RemovePlayerActivity;
+import at.footballapp.captainandroid.footballapp.pkgGUI.UpdateMatchActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,8 +16,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-
-    public void onImgBtn(View view) {
+        //test comment for first commit
+        //what is all the code about ?
+        Intent i = new Intent(this, EditTeamActivity.class);
+        startActivity(i);
     }
 }
