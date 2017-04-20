a2b3c22aaf2736c20678d382c19b71e4086ab568
Sasa <sarkovicsasa123@gmail.com>
2017-04-20 14:31:20 +0200

    MatchActivity added

---
+++ app/src/main/java/at/footballapp/captainandroid/footballapp/pkgGUI/MatchActivity.java
@@ -0,0 +1,15 @@
+package at.footballapp.captainandroid.footballapp.pkgGUI;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import at.footballapp.captainandroid.footballapp.R;
+
+public class MatchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_match);
+    }
+}
