2d93a48e13a13b8c8dc73820bb8abde2a23bdddb
Sasa <sarkovicsasa123@gmail.com>
2017-04-26 20:11:03 +0200

    Navigation.xml added (overflow menu button)

--- app/src/main/java/at/footballapp/captainandroid/footballapp/pkgGUI/MainActivity.java
+++ app/src/main/java/at/footballapp/captainandroid/footballapp/pkgGUI/MainActivity.java
@@ -2,6 +2,7 @@ package at.footballapp.captainandroid.footballapp.pkgGUI;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.View;
 
 import at.footballapp.captainandroid.footballapp.R;
@@ -12,5 +13,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.navigation, menu);
+        return true;
     }
 }
