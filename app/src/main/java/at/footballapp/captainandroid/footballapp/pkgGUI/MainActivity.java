0767b63b95da54374d60cfecd96ca1cc758f57a5
JakobWeiler <jakob.weiler@edu.htl-villach.at>
2017-04-26 22:39:58 +0200

    Menu updated Activity calls added

--- app/src/main/java/at/footballapp/captainandroid/footballapp/pkgGUI/MainActivity.java
+++ app/src/main/java/at/footballapp/captainandroid/footballapp/pkgGUI/MainActivity.java
@@ -1,9 +1,15 @@
 package at.footballapp.captainandroid.footballapp.pkgGUI;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
 
 import at.footballapp.captainandroid.footballapp.R;
 
@@ -13,11 +19,45 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Spinner spPlayer = (Spinner) findViewById(R.id.spPlayer);
+        ArrayList<String> player = new ArrayList<String>();
+        player.add("Player");
+        player.add("Some players");
+        ArrayAdapter<String> adapterPlayer = new ArrayAdapter<String>(
+                this,
+                android.R.layout.simple_spinner_item,
+                player
+        );
+
+        spPlayer.setAdapter(adapterPlayer);
+
+        Spinner spMatch = (Spinner) findViewById(R.id.spMatch);
+        ArrayList<String> match = new ArrayList<String>();
+        match.add("Match");
+        match.add("Some matches");
+        ArrayAdapter<String> adapterMatch = new ArrayAdapter<String>(
+                this,
+                android.R.layout.simple_spinner_item,
+                match
+        );
+
+        spMatch.setAdapter(adapterMatch);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.navigation, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.navigation_profile: startActivity(new Intent(MainActivity.this, ProfileActivity.class)); break;
+            case R.id.navigation_match: startActivity(new Intent(MainActivity.this, MatchActivity.class)); break;
+            case R.id.navigation_statistic: startActivity(new Intent(MainActivity.this, StatisticActivity.class)); break;
+        }
         return true;
     }
 }
