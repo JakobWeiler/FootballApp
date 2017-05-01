4351380185ecdfb44140a7bfdadc6d2a617f9e60
JakobWeiler <jakob.weiler@edu.htl-villach.at>
2017-05-01 22:34:06 +0200

    Launcher icon updated Navigation drawer added

--- app/src/main/java/at/footballapp/captainandroid/footballapp/pkgGUI/MainActivity.java
+++ app/src/main/java/at/footballapp/captainandroid/footballapp/pkgGUI/MainActivity.java
@@ -1,12 +1,17 @@
 package at.footballapp.captainandroid.footballapp.pkgGUI;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.view.ActionMode;
+import android.support.design.widget.Snackbar;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
@@ -14,12 +19,24 @@ import java.util.ArrayList;
 
 import at.footballapp.captainandroid.footballapp.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
 
         Spinner spPlayer = (Spinner) findViewById(R.id.spPlayer);
         ArrayList<String> player = new ArrayList<String>();
@@ -47,19 +64,41 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.navigation, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.navigation_profile: startActivity(new Intent(MainActivity.this, ProfileActivity.class)); break;
-            case R.id.navigation_match: startActivity(new Intent(MainActivity.this, MatchActivity.class)); break;
-            case R.id.navigation_statistic: startActivity(new Intent(MainActivity.this, StatisticActivity.class)); break;
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
         }
-        return true;
     }
 
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_addPlayer) {
+
+        } else if (id == R.id.nav_removePlayer) {
+            startActivity(new Intent(MainActivity.this, RemovePlayerActivity.class));
+        } else if (id == R.id.nav_Profile) {
+            startActivity(new Intent(MainActivity.this, ProfileActivity.class));
+        } else if (id == R.id.nav_addMatch) {
+
+        } else if (id == R.id.nav_removeMatch) {
+            startActivity(new Intent(MainActivity.this, RemoveMatchActivity.class));
+        } else if (id == R.id.nav_updateMatch) {
+            startActivity(new Intent(MainActivity.this, UpdateMatchActivity.class));
+        } else if (id == R.id.nav_showMatch) {
+            startActivity(new Intent(MainActivity.this, MatchActivity.class));
+        } else if (id == R.id.nav_showStatistic) {
+            startActivity(new Intent(MainActivity.this, StatisticActivity.class));
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
 }
