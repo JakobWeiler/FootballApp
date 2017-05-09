84efc9b5ac0aeafae3f682c75d7e0af59ba7f636
JakobWeiler <jakob.weiler@edu.htl-villach.at>
2017-05-09 10:23:19 +0200

    Test data in RemovePlayer Admin checkbock in dialog_add_player added

--- app/src/main/java/at/footballapp/captainandroid/footballapp/pkgGUI/RemovePlayerActivity.java
+++ app/src/main/java/at/footballapp/captainandroid/footballapp/pkgGUI/RemovePlayerActivity.java
@@ -2,8 +2,14 @@ package at.footballapp.captainandroid.footballapp.pkgGUI;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import at.footballapp.captainandroid.footballapp.R;
+import at.footballapp.captainandroid.footballapp.pkgData.Player;
 
 public class RemovePlayerActivity extends AppCompatActivity {
 
@@ -11,5 +17,17 @@ public class RemovePlayerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_remove_player);
+
+        ListView lvPlayer = (ListView) findViewById(R.id.lvPlayer);
+        ArrayList<String> player = new ArrayList<String>();
+        player.add("Player");
+        player.add("Some players");
+        ArrayAdapter<String> adapterPlayer = new ArrayAdapter<String>(
+                this,
+                android.R.layout.simple_spinner_item,
+                player
+        );
+
+        lvPlayer.setAdapter(adapterPlayer);
     }
 }
