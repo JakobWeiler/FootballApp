7279dc5a54669bf98e946c8ef59a9d2863579566
c-p-wutti <christopher.wutti@gmx.at>
2017-06-01 15:44:48 +0200

    Finished Update Match (Participation etc.)

--- app/src/main/java/at/footballapp/captainandroid/footballapp/pkgData/Player.java
+++ app/src/main/java/at/footballapp/captainandroid/footballapp/pkgData/Player.java
@@ -88,8 +88,6 @@ public class Player implements Serializable{
         return password;
     }
 
-    public ArrayList<EnumPositions> getPositions(){ return positions;}
-
     public void setPassword(String password) {
         this.password = password;
     }
